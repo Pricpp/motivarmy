b4713bf7c55553274fc6efbd561c57feb67bc0bc
Pricpp <priscillapaula2608@hotmail.com>
2026-02-01 00:57:40 -0300

    Implement service worker with caching functionality
    Add service worker for caching assets and offline support

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
-self.addEventListener('fetch', function(event) {});
+const cacheName = 'motivarmy-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon.png'
+];
+
+// Instala o Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// Faz o app funcionar offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
