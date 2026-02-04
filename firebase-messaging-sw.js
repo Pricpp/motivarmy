7647ef7bf4e4bd7440ac5dce19b151a0c5746a30
Pricpp <priscillapaula2608@hotmail.com>
2026-02-04 13:08:05 -0300

    Implement background message handling for notifications
    Added Firebase Cloud Messaging background message handler.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,20 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-sw.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-sw.js');
 
 firebase.initializeApp({
-    messagingSenderId: "205652769161"
+    apiKey: "AIzaSyB8cfcxxPVCaL0JzqvBLQYcnILsHsyGVhc",
+    projectId: "motivarmy-53e34",
+    messagingSenderId: "205652769161",
+    appId: "1:205652769161:web:b1e7936ed4f1a20f21c958"
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: 'icon.png'
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
